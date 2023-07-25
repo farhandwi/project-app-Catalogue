e5c9852ca33232a395c556585a269c9cadbe1aaa
GOLDENWISE <abdamadhafiz13@gmail.com>
2023-07-25 22:38:22 +0700

    revisi error ke 2

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
-<script src="js/bootstrap.bundle.min.js"></script>
-    <script src="@popperjs/core/dist/umd/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+<script src="{{ env('APP_URL') }}/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ env('APP_URL') }}/@popperjs/core/dist/umd/popper.min.js"></script>
+    <script src="{{ env('APP_URL') }}/js/bootstrap.min.js"></script>
   </body>
 </html>
